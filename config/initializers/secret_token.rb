5df7b15124c8c75abdf673e5490180972241506b
Cameron Dutro <cdutro@twitter.com>
2012-08-02 09:22:40 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chopstick::Application.config.secret_token = '224d6471c7fca222083bf9392414e785d4f6fd60fdf8489ec47ff43ee517b2b0091c98b03ea15005b2089eef70ac063df3be4427a47179504f6a757d365e2fb6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Chopstick::Application.config.session_store :cookie_store, :key => '_chopstick_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Chopstick::Application.config.session_store :active_record_store
